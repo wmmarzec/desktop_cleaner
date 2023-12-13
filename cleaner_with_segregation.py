b54bbb6f60647bea05a324d78c11ca1115ba3af7
Wiktoria Marzec <wiktoria.m.marzec@gmail.com>
2023-12-13 11:47:10 +0100

    cleaner_with_segregation

--- cleaner_with_segregation.py
+++ cleaner_with_segregation.py
@@ -1,25 +1,48 @@
 import os
 import shutil
-import datetime
 
-def create_dir(cleanup_dir, x):
-    cleanup_dir = r"C:\Users\wikto\OneDrive\Pulpit/" + x
+def create_dir(cleanup_dir):
     if not os.path.exists(cleanup_dir): 
         os.makedirs(cleanup_dir)
         last_part = cleanup_dir.split("/")
         print ("Creating folder " + last_part[1] + "...")
     return cleanup_dir
 
+def get_necessary_dir(new_directories, file):
+    necessary_dir = False
+    for i in new_directories:
+        if i == file:
+            necessary_dir = i
+    return necessary_dir
+
+def get_dir_name (ext):
+    dir_type = ""
+    if ext == "bmp" or "gif" or "ico" or "jpeg" or "jpg" or "png" or "raw" or "tif" or "tiff":
+        dir_type = "Images"
+    elif ext == "doc" or "docx" or "odt" or "pages" or "rtf" or "txt" or "wpd" or "wps":
+        dir_type = "Texts"
+    elif ext == "aif" or "mp3" or "wav" or "wma":
+        dir_type = "Music"
+    elif ext == "mov" or "mp4" or "mpg" or "wmv":
+        dir_type = "Videos"
+    dir_name = desktop_dir + "/" + dir_type
+    new_directories.append(dir_type)
+    return dir_name
+
+
 
 desktop_dir = r"C:\Users\wikto\OneDrive\Pulpit"
 
 all_files = os.listdir(desktop_dir)
+new_directories = []
 
 for file in all_files:
-
-    if file != __file__ and file!= "standard_cleaner.bat":
+    necessary_dir = get_necessary_dir(new_directories, file)
+    if file != __file__ and file!= "standard_cleaner.bat" and file != necessary_dir:
         file_dir = desktop_dir + "/" + file
-        x = ""
-        cleanup_dir = create_dir(cleanup_dir, x)
-        shutil.move(file_dir, cleanup_dir)
+        file = file.split()
+        ext = file[len(file)-1]
+        cleanup_dir_name = get_dir_name (ext)
+        cleanup_dir = create_dir(cleanup_dir_name)
+        #shutil.move(file_dir, cleanup_dir)
         print ("Moving file " + file + "...")
